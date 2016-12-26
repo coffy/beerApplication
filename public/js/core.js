7f577a12301bc5e61e9ee6330df9cd17e4b42af9
gilfell87 <gil.m.fell@gmail.com>
2016-12-26 11:02:27 +0000

    load client side

--- public/js/core.js
+++ public/js/core.js
@@ -2,18 +2,42 @@ import React from 'react';
 import { render } from 'react-dom';
 import AppBar from 'material-ui/AppBar';
 import MuiTheme from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+//import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import ScoreList from './components/ScoreList';
+//import Avatar from 'material-ui/Avatar';
+import FlatButton from 'material-ui/FlatButton';
+import BeerProfileCreate from './components/BeerProfileCreate'
+
 
 
 
 window.onload = () => {
-  render(
-    <MuiTheme>
-      <div>
-        <AppBar title="Score my Beer"
-                showMenuIconButton={false}/>
-        <ScoreList/>
-      </div>
-    </MuiTheme>
-    ,document.getElementById('react-app'));
+
+
+    render(
+        <MuiTheme>
+            <div>
+                <AppBar title="Score my Beer"
+                        showMenuIconButton={false}
+                        iconElementRight={
+                            <FlatButton
+                                label="Add tasting profile"
+                                onClick = {() => {
+
+                                    var beerProfile = render(<BeerProfileCreate/>, document.getElementById('react-create'));
+
+                                    beerProfile.handleOpenModal();
+
+                                }}
+
+                            />
+
+                            }
+                >
+                </AppBar>
+                <ScoreList loadedBeers={[]}/>
+            </div>
+        </MuiTheme>
+        ,document.getElementById('react-app'));
 };
